54560f7a09b85a00e288d83018c981fc2a23b46e
Gavin56 <gavinobrien56@gmail.com>
2020-10-06 13:11:29 -0400

    First commit. Added CSS to provide basic purple buttons for aesthetic. Added Time: to the timer and placed it in the right corner to make it more understandable and aesthetically pleasing.

---
+++ README copy.md
@@ -0,0 +1,37 @@
+# 04 Web APIs: Code Quiz
+
+As you proceed in your career as a web developer, you will probably be asked to complete a coding assessment, which is typically a combination of multiple-choice questions and interactive challenges. Build a timed code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
+## Task
+
+```
+Create a quiz on javascript fundamentals and keep track of high scores.
+```
+
+## Requirements
+
+```
+Once the user starts the quiz, present the first question and start the coutdown timer.
+When the user selects an answer, pause the quiz timer and let the user know if the answer right/wrong.
+If the user is wrong, subtract penalty time from the clock.
+After 2 seconds, proceed to the next question and resume the countdown timer.
+If the countdown timer reaches 0 the quiz is over.
+If the user answers all the questions the quiz is over.
+The user's score and initials are then saved after which the high scores will be displayed.
+Also provide a link straight to viewing the high scores page.
+```
+
+The following animation demonstrates the application functionality:
+
+![code quiz](./Assets/04-web-apis-homework-demo.gif)
+
+### Review
+
+You are required to submit the following for review:
+
+* The URL of the functional, deployed application.
+
+* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+
+- - -
+© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
---
+++ script.js
@@ -0,0 +1,114 @@
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question:
+      "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+];
+
+var questionEl = document.querySelector("#question");
+var optionListEl = document.querySelector("#option-list");
+var questionResultEl = document.querySelector("#question-result");
+var timerEl = document.querySelector("#timer");
+
+var questionIndex = 0;
+var correctCount = 0;
+var time = 20;
+var intervalId;
+
+// ## Instructions
+// 1. At the end of the quiz, ask the user for their name.
+// 2. Store their name and score in localstorage.
+// 3. Show the highscore list in descending order (You will need to reserach the sort function for Arrays)
+
+function endQuiz() {
+  clearInterval(intervalId);
+  var body = document.body;
+  body.innerHTML = "Game over, You scored " + correctCount;
+
+  setTimeout(showHighScore, 2000);
+  // wait 2 seconds and call showHighScore;
+}
+
+function showHighScore() {
+  // write code here
+  var user = prompt("What is your name?");
+  var userArray = [];
+
+  localStorage.setItem("user", user);
+  localStorage.setItem("highScore", correctCount);
+
+  var userObj = {
+    name: user,
+    score: correctCount
+  }
+
+  userArray.push(userObj);
+  userArraysort((a,b)=>a-b);
+
+  console.log(userArray);
+
+}
+
+function updateTime() {
+  time--;
+  timerEl.textContent = "Time: " + time;
+  if (time <= 0) {
+    endQuiz();
+  }
+}
+
+function renderQuestion() {
+  if (time == 0) {
+    updateTime();
+    return;
+  }
+  
+  intervalId = setInterval(updateTime, 1000);
+  questionEl.textContent = questions[questionIndex].question;
+
+  optionListEl.innerHTML = "";
+  questionResultEl.innerHTML = "";
+
+  var choices = questions[questionIndex].choices;
+  var choicesLenth = choices.length;
+
+  for (var i = 0; i < choicesLenth; i++) {
+    var questionListItem = document.createElement("li");
+    questionListItem.textContent = choices[i];
+    optionListEl.append(questionListItem);
+  }
+}
+
+function nextQuestion() {
+  questionIndex++;
+  if (questionIndex === questions.length) {
+    time = 0;
+  }
+  renderQuestion();
+}
+
+function checkAnswer(event) {
+  clearInterval(intervalId);
+  if (event.target.matches("li")) {
+    var answer = event.target.textContent;
+    if (answer === questions[questionIndex].answer) {
+      questionResultEl.textContent = "Correct";
+      correctCount++;
+    } else {
+      questionResultEl.textContent = "Incorrect";
+      time = time - 2;
+      timerEl.textContent = time;
+    }
+  }
+  setTimeout(nextQuestion, 2000);
+}
+
+renderQuestion();
+optionListEl.addEventListener("click", checkAnswer);
